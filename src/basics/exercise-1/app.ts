fb18c13aabe9657494ebbaba5d88cda7f9762307
SantiagoGG0 <santiagogarcia337@gmail.com>
2023-05-03 21:27:00 -0500

    Pilares de la programacion (Abstraccion) y (Encapsulamiento)

--- src/basics/exercise-1/app.ts
+++ src/basics/exercise-1/app.ts
@@ -42,8 +42,65 @@ let students:Array<string | null> = ['Santiago', 'Maria', null];
 
 console.log( students.filter((students) => students !== null ) );
 
-// Tuplas.
+// Tuples.
 let exampleTuple:[string, null] = ['hola', null];
 
 type statusCode = 'active' | 'inactive';
 let studentStatus: statusCode = 'inactive';
+
+// Any 
+let working: any = 25;
+working = 'hola';
+
+// Funciones.
+/**function nombreFunción(parámetro: tipoDato): tipoDatoQueRetornaLaFunción{}
+ *  */ 
+function validateValue( value: unknown ): boolean {
+
+    // Unknown que es un tipo de dato que es desconocido.
+    // Para cualquier validación que deba hacer con el dato.
+    // Debo primero saber o validar que tipo de dato es.
+    if ( typeof value ==='string') {
+        console.log(value.trim().toUpperCase()); 
+    }
+        else if (typeof value == 'number'){
+            console.log(value.toFixed());
+    }
+
+    if (value === ''){
+        return false;
+    }   else {
+        return true;
+    }
+}
+
+// Void.
+function processResponse (saved: boolean){
+    if(saved){
+        console.log('Guardado');
+    } else {
+        console.log('Ocurrió un error');
+    }
+}
+
+enum Role {
+    Admin,
+    Client,
+}
+
+let users: Array < { name: string, role: Role } > = [
+    {
+        name: 'Pepe',
+        role:  Role.Admin
+    },
+    {
+        name: 'Juan',
+        role: Role.Client
+    },
+    {
+        name: 'Fulano',
+        role: Role.Client
+    }
+]
+
+console.log( users.filter( (user) => user.role === Role.Client) );
---
+++ src/basics/exercise-2/poo-basic.ts
@@ -0,0 +1,61 @@
+class Auto {
+    
+    // Atributos de la clase o propiedades.
+    color: string = 'Negro'; // Inicializar directamente.
+    puertas: number;        // Inicializar el constructor.
+    marca?: string;         // Marcar como opcionales
+    // Primer método que se ejecuta en una instancia de la clase.
+    // Usado para inicializar propiedades de una clase.
+    constructor(marca?: string) {
+        this.puertas = 4;      // Con this, accedo a todas las propiedades y métodos de la clase.
+        this.marca = marca;
+
+    }
+    // Métodos de la clase.
+    acelerar(){
+        console.log('Acelerando');
+    }
+    frenar(){
+        console.log('Frenando');
+    }
+    prender(){
+        console.log('Prendiendo');
+    }
+}
+let Ferrari: Auto = new Auto(); // Instancia de la clase Auto - Objeto.
+console.log(Ferrari);
+Ferrari.marca = 'Ferrari';
+console.log(Ferrari);
+let lambo = new Auto(); 
+console.log(lambo);
+lambo.color = 'Exótico';
+lambo.marca = 'Lamborghini';
+lambo.puertas = 2;
+console.log(lambo);
+
+class Auto2 {
+
+    color: string = 'Negro'; // Implícita Por defecto es publico.
+    public puertas:  number; // Forma Explícita para colocar una propiedad publica.
+    
+    // En el constructor yo puedo crear propiedades de la clase
+    // Si y solo si tienen el modificador de acceso (public, private, protected)
+
+    // Los parámetros que son opcionales, siempre deben ir al final de cualquier método o función
+    // En este caso el constructor es un método.
+    constructor(public peso: number, public marca?: string){
+        this.puertas = 4;
+        this.encender();
+        
+    }
+    apagar() {
+        console.log('Apagando');
+    }
+    encender() {
+        console.log(` Bienvenido ${this.marca}, tu color es: ${this.color}, tu peso es: ${this.peso} y tienes ${this.puertas} puertas.`);
+    }
+}
+
+let ford = new Auto2(500, 'Ford');
+console.log(ford);
+ford.apagar();
